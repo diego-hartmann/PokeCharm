8ee6b4094479405f55d3082687ca24fd30aafbf4
diego-hartmann <dtghartmann@gmail.com>
2023-02-28 07:51:10 -0300

    starting to create the pokemon singlepage

---
+++ src/pages/PokemonPage.tsx
@@ -0,0 +1,32 @@
+interface IPokemonInfo{
+    name : string,
+    image : string,
+    id : number,
+    order : number,
+    height : number,
+    weight : number,
+    base_experience : number,
+    abilities : any[],
+    stats : any[],
+}
+interface IPokemon{
+    pokemon : IPokemonInfo,
+}
+
+const PokemonPage = ( { pokemon } : IPokemon ) => {
+  return (
+    <>
+        <div>Name: {pokemon.name}</div>
+        <img src={pokemon.image} />
+        <div>Id: {pokemon.id}</div>
+        <div>Order: {pokemon.order}</div>
+        <div>Height: {pokemon.height}</div>
+        <div>Weight: {pokemon.weight}</div>
+        <div>Base experience: {pokemon.base_experience}</div>
+        <div>Abilities: {pokemon.abilities}</div>
+        <div>Stats: {pokemon.stats}</div>
+    </>
+  )
+}
+
+export default PokemonPage
