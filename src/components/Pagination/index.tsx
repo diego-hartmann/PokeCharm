faf602c8a3ab676828fa1e7777993ad9dbad573f
diego-hartmann <dtghartmann@gmail.com>
2023-02-28 04:27:15 -0300

    react paginate

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,63 @@
+// This component is imported from https://github.com/AdeleD/react-paginate
+// I am not removing the original comments from this file, just refactor some code.
+
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
+// Example items, to simulate fetching from another resources.
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }:any) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item:any) => (
+          <div>
+            <h3>Item #{item}</h3>
+          </div>
+        ))}
+    </>
+  );
+}
+
+interface IProps { itemsPerPage : number }
+
+const Pagination = ({ itemsPerPage }:IProps) => {
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = ( event : any ) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={()=>{}}
+      />
+    </>
+  );
+}
+
+export default Pagination;
--- src/components/PokeText/index.tsx
+++
@@ -1,3 +0,0 @@
-import css from './style.module.css';
-const PokeText = (props:{text:string}) => <p className={css.pokeText}>{props.text}</p>
-export default PokeText;
