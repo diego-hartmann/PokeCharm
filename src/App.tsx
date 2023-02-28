6115dfc09070c4683ef4ff01b53ed3e0abb5fe9f
diego-hartmann <dtghartmann@gmail.com>
2023-02-28 04:09:06 -0300

    laoder

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,21 @@ import './App.css';
 import Loader from './components/Loader';
 import usePokeList from './hooks/usePokeList';
 import Card from './components/Card';
+import { Suspense } from 'react';
+
 function App() {
 
-  const { pokemons, isLoading, errorMessage, isSuccess } = usePokeList(33);
+  const { pokemons, isLoading, errorMessage, isSuccess } = usePokeList();
 
-
-  
   return (
     <div className="App">
-        <>
-        {isLoading && <Loader/>}
-        
-        {isSuccess && pokemons.map( (pokemon:any, index:number) => {
-          console.log(pokemon)
-          return(
-            <Card key={index} pokemon={pokemon}/>
-          )  
-        }) }
+      <Loader message='Getting Pokemóns list'/>
+        {/* {
+          isLoading ? <Loader message='Getting Pokemóns list'/>
+          : isSuccess && pokemons.map( ( pokemon:any, index:number ) => <Card key={index} pokemon={pokemon}/> )
+        } */}
 
-        </>
+
     </div>
 
   );
