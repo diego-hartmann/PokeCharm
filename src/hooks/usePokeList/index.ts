3c078017fbb48bc4535128091ff38413b7b91b23
diego-hartmann <dtghartmann@gmail.com>
2023-03-02 12:24:27 -0300

    tests working, removed axio

--- src/hooks/usePokeList/index.ts
+++ src/hooks/usePokeList/index.ts
@@ -7,130 +7,75 @@
 // importing the hooks that are going to be bootstraped
 import { useState, useEffect } from 'react';
 
-// dealing with axios
-import axios, { CancelTokenSource } from 'axios';
-
-// importing custom types
-
 // types
 import { IRequestResult } from '../../data/@types/IRequestResult';
 import { IPokemon } from '../../data/@types/IPokemon';
 
-// importing the localstorage pokemons so it dows not over-use request.
+// local storage cached pokemons (not working)
 import { cashedPokemons } from '../../utils/cashedPokemons';
 
-
 /**
- * Hook for making GET requests to POKEAPI. 
- * @param count the number of pokemons to be displayed.
- * @returns an object {isLoading, pokemons, isSuccess, errorMessage} holding the states of the request.
+ * A custom hook for fetching a list of Pokemons from the PokeAPI
+ * @param count The number of Pokemons to fetch
+ * @returns An object containing the state of the request
  */
-const usePokeList = ( count : number ) => {
+const usePokeList = (count: number) => {
+  // Define state variables for Pokemons, loading, success, and error message
+  const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
+  useEffect(() => {
+    // Check if there are cached Pokemons in local storage and use them if they exist
+    const cashed = cashedPokemons.get();
+    if (cashed.length > 0) {
+      setPokemons(cashed);
+      setIsSuccess(true);
+      setIsLoading(false);
+      setErrorMessage('');
+      console.log('Cached Pokemons');
+      return;
+    }
 
+    console.log('Requesting Pokemons...');
 
+    // Create an array of Promises that will fetch the Pokemons from the PokeAPI
+    const requests = [];
+    for (let i = 1; i <= count; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      requests.push(fetch(url));
+    }
 
-    //#region ==============-- HOLDING THE STATES --=====================================================
-    // Bellow, even though not needed, I will explicitly declare the types of the generic parameters.
-    // This way, I make my intention clear.
-    // Also, it will prevent any error by changing the default value of the useState parameter by mistake.
-    
-    // holds the state for the request data result. Starts as an empty list.
-    const [ pokemons, setPokemons ] = useState<IPokemon[]>([]);
-    
-    // specifies if the request is in loading process or not. Starts as true.
-    const [ isLoading, setIsLoading ] = useState<boolean>(true);
-
-    // specifies if the request is succeeded or not. Starts as true.
-    const [isSuccess, setIsSuccess] = useState<boolean>(false);
-    
-    // holds the state for the error message from the request. Starts as an empty string.
-    const [errorMessage, setErrorMessage] = useState<string>('');
-    //#endregion ________________________________________________________________________________________
-    
-
-
-
-
-
-    //#region ==============-- DEALING WITH THE FETCH LOGIC --===========================================
-    useEffect( ()=> {
-
-        
-        // if there is pokemon inside localstorage, let's save reqs. Or payed servers, it saves money.
-        // for some reason, it does not seem to work... Maybe due to filesize, it does not save into localstorage
-        const cashed = cashedPokemons.get();
-        if(cashed.length > 0){
-            setPokemons(cashed);
-            setIsSuccess(true);
-            setIsLoading(false);
-            setErrorMessage('');
-            console.log("Cashed pokemóns");
-            return;
+    // Use Promise.all to wait for all the Promises to resolve
+    Promise.all(requests)
+      .then(async (responses) => {
+        // Parse the response data and store the Pokemons in state
+        const pokeList = [];
+        for (const response of responses) {
+          if (response.ok) {
+            const pokemon = await response.json();
+            pokeList.push(pokemon);
+          } else {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
         }
+        setPokemons(pokeList);
+        cashedPokemons.set(pokeList);
+        setIsSuccess(true);
+        console.log('Success on request.');
+      })
+      .catch((error) => {
+        // If an error occurs, store the error message in state
+        setErrorMessage(`An error occurred on GET Pokemons:\n${error}`);
+        setIsSuccess(false);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
 
-        console.log("Requesting pokemóns...");
-
-        // creating axios source so we can get its token and cancel() it into unMount (return()=>...).
-        const axiosSource : CancelTokenSource = axios.CancelToken.source();
-        
-        // axios promise config
-        const config = { cancelToken: axiosSource.token };
-
-        // bootstraping the axios promisse into another function for organization and maintaince. 
-        // const makeReq : TPromiseToAxiosResponse = async () => await axios.get( url, config );
-
-        let promises = [];
-        for (let i = 1; i <= count; i++) {
-            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-            promises.push(axios.get(url, config));
-        }
-
-        // executing the request once the hook is called by the caller Component.
-        Promise.all(promises)
-        // defining the data state based on the response data.
-        .then( ( requests : any )  => { 
-            // accessing the list of pokemons
-            const pokeList = requests.map( ( req : any ) => req.data );
-            
-            // saving it into the global state context.
-            setPokemons(pokeList);
-            
-            // saving it into storage, so there is no need to re-request in other render.
-            // for some reason, it does not seem to work...
-            cashedPokemons.set(pokeList)
-            
-            setIsSuccess(true);
-    
-            console.log("Success on request.");
-        })
-        // defining the error message based on the enpoint.
-        .catch( error => {
-            setErrorMessage( `An error ocurred on GET pokemons:\n${error}` );
-            setIsSuccess( false );
-        })
-        // cancelling loading once the request is over, regardless its result.
-        .finally( ()=> setIsLoading( false ) );
-
-        // canceling the source once the component unmounts.
-        return () => axiosSource.cancel();
-
-    }, [] )
-    //#endregion ________________________________________________________________________________________
-
-
-
-
-
-
-    //#region ==============-- RETURNING THE OBJECT WITH THE STATES --===================================
-    // returning all the states at one object to be updated into the caller Component after finishing the useEffect callback.
-    const resultData : IRequestResult = { pokemons, isLoading, isSuccess, errorMessage }; 
-    return resultData;
-    //#endregion ________________________________________________________________________________________
-
-
-
-}
+  // Return an object containing the state variables
+  const resultData: IRequestResult = { pokemons, isLoading, isSuccess, errorMessage };
+  return resultData;
+};
 
 export default usePokeList;
